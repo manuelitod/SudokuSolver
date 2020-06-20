ba03fce9f481423af11f3d0fa36ffb6fa6b77c10
IanKz <14-10406@usb.ve>
2020-06-19 23:33:23 -0400

    Agregadas las funciones que escriben en ficheros el formato de entrada del resolvedor de SAT y el formato de salida del sudoku resuelto.

--- helpers/sudoku.py
+++ helpers/sudoku.py
@@ -1,6 +1,7 @@
 # Modulo de funciones de utilidad para la clase soduku
 
 from classes.sudoku import Soduku
+from classes.translator_txt_cnf import Literal
 
 # Dada una representacion en string del soduku
 # devuelve una instancia de soduku
@@ -12,7 +13,7 @@ def create_soduku_instance(soduku_string):
 	return Soduku(board, size)
 
 # Dado un archivo soduku se crea una lista
-# de instancias de sodoku
+# de instancias de sudoku
 def create_soduku_array_instances(soduku_filename):
 	soduku_file = open(soduku_filename, 'r')
 	soduku_array_instances = []
@@ -20,3 +21,64 @@ def create_soduku_array_instances(soduku_filename):
 		# To-do Checkear si la linea es valida
 		soduku_array_instances.append(create_soduku_instance(soduku_line))
 	return soduku_array_instances
+
+# Método que lee un fichero que contiene la salida de un resolvedor de SAT y
+# lo traduce a un string que contenga la solución del sudoku.
+
+def get_sudoku_from_sat(sol_filename, translator):
+
+	sudoku = ''
+	literals = translator.literals
+	fd = open(sol_filename, 'r')
+	counter = 0
+
+	for line in fd:
+
+		if counter == 0:
+
+			counter = 1
+			satisfy = line.split(' ')[2]
+
+			if int(satisfy) != 1:
+
+				print("Esto se jodio, compadre.")
+				return
+			continue
+
+		literal = line.split(' ')[1]
+
+		if int(literal) > 0:
+
+			sudoku = sudoku + str(literals[int(literal)-1].sodoku_value)
+
+	fd.close()
+	return sudoku
+
+# Método que escribe el fichero que contiene la solución del sudoku.
+
+def write_sudoku_sol(solution, filename):
+
+	fd = open(filename, 'a')
+
+	fd.write(solution)
+	fd.close()
+
+def write_sat_format(preps, lits, filename):
+
+	file = 'sat_' + filename.split('/')[-1]
+	fd = open(file, 'a')
+	prologue = 'p cnf ' + str(lits) + ' ' + str(len(preps)) + '\n'
+
+	fd.write(prologue)
+
+	for i in range(0, len(preps)):
+
+		line = ''
+
+		for j in preps[i]:
+			line = line + str(j) + ' '
+
+		line = line + '0\n'
+		fd.write(line)
+
+	fd.close()
